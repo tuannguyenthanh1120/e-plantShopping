4ae1968bfccb88ba5a5b69c73a5f7dc0599c43b9
tuannguyenthanh1120 <tuannguyenthanh.work@gmail.com>
2025-06-28 06:14:32 -0400

    initial commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -13,6 +13,10 @@ function ProductList({ onHomeClick }) {
     [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
   }));
 };
+    const calculateTotalQuantity = () => {
+ return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+  };
+  
     const plantsArray = [
         {
             category: "Air Purifying Plants",
